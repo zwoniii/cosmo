82a1ec33e8a160add1e11546d4926321117c7b67
irazv <zwoniii@ya.ru>
2023-04-28 09:50:38 +0300

    slider css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+    $('.slider__box').slick({
+        prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="left arrow">',
+        nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="right arrow">',
+    });
 });
