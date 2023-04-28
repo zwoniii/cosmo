338c4663a3fa6412d5b7a41ed779d503f57a6596
irazv <zwoniii@ya.ru>
2023-04-28 13:03:45 +0300

    header adaptive

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function(){
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="left arrow">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="right arrow">',
     });
+
+    $('.menu-btn').on('click', function() {
+        $('.menu__list').toggleClass('active')
+    })
 });
