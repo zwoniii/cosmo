74a097200b316bdea4e5c56be4d7cb33e3c18110
irazv <zwoniii@ya.ru>
2023-04-28 14:23:03 +0300

    info adaptive

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="left arrow">',
         nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="right arrow">',
+        responsive: [
+            {
+                breakpoint: 481,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 
     $('.menu-btn').on('click', function() {
